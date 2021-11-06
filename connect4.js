356ad070ed268f3fcce2f5ae2d939cf8e8d3d65c
Stephanie <smmackin12@gmail.com>
2021-11-06 13:58:53 -0400

    uploading game

--- connect4.js
+++ connect4.js
@@ -18,7 +18,7 @@ let board = []; // array of rows, each row is array of cells  (board[y][x])
 function makeBoard() {
    // TODO: set "board" to empty HEIGHT x WIDTH matrix array
   for (let y = 0; y <= HEIGHT; y++){
-       board.push(Array.from({HEIGHT,WIDTH}));
+       board.push(Array.from({length: WIDTH}));
  }
 }
 
@@ -26,10 +26,10 @@ function makeBoard() {
 
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
-   const htmlBoard = document.getElementById('board');
+   const board = document.getElementById('board');
 
   // TODO: add comment for this code -- after finding the table in the HTML by the element Id #board.   top variable is storing the first table row for the game board.   The tr is then given the id of 'column-top'.  We are then adding an event listener to 'top'.  The event listener is handling the user click on the target 'top'. 
-  let top = document.createElement("tr"); 
+  const top = document.createElement("tr"); 
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
 
@@ -39,7 +39,7 @@ function makeHtmlBoard() {
     headCell.setAttribute("id", x);
     top.append(headCell);
   }
-  htmlBoard.append(top);
+  board.append(top);
 
   // TODO: add comment for this code -- creating rows by looping each time through 'HEIGHT-6' and each time creating a new tr element.  
   for (let y = 0; y < HEIGHT; y++) {
@@ -52,27 +52,39 @@ function makeHtmlBoard() {
       row.append(cell);
     }
     // rows are added to the top-column
-    htmlBoard.append(row);
+    board.append(row);
   }
 }
-
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 0
-  return 0;
-}
+  for (let y= HEIGHT-1; y>0; y--){
+    if(!board[y][x]){
+      return y; 
+    }
+  }
+  return null;
+};
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
+ const piece = document.createElement('div'); 
+ piece.classList.add('piece');
+ piece.classList.add(`p${currPlayer}`);
+ piece.style.top = -50 * (y+2);  /// ?????
+
+ const spot = document.getElementById(`${y}-${x}`);
+  spot.append(piece);
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
   // TODO: pop up alert message
+  alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -89,6 +101,7 @@ function handleClick(evt) {
 
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
+  board[y][x] = currPlayer;
   placeInTable(y, x);
 
   // check for win
@@ -98,9 +111,13 @@ function handleClick(evt) {
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
+ if (board.every(row => row.every(cell => cell))){ 
+   return endGame('Tie');
+ }
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  // TODO: switch currPlayer 1 <-> 2 ...currPlayer is ===1, or 2 if not 1.  
+  currPlayer = currPlayer === 1 ? 2 : 1; /// why would let or const not work in this situation?  
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
